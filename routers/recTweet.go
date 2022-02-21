e86c94a52e108647e59845be8c93ddf354168ef2
RodrigoGonzalez78 <61030017+RodrigoGonzalez78@users.noreply.github.com>
2022-02-21 15:20:16 -0300

    Tweet 2

--- routers/recTweet.go
+++ routers/recTweet.go
@@ -1,7 +1,37 @@
 package routers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+	"twitter_clone_backEnd/bd"
+	"twitter_clone_backEnd/models"
+)
 
 func RecTweet(w http.ResponseWriter, r *http.Request) {
+	var message models.Tweet
 
+	err := json.NewDecoder(r.Body).Decode(&message)
+
+	if err != nil {
+		http.Error(w, "Cuerpo del tweet invalido!"+err.Error(), 400)
+	}
+
+	register := models.GrabarTweet{
+		UserId:  IDUser,
+		Message: message.Message,
+		Date:    time.Now(),
+	}
+
+	_, status, err := bd.InsertTweet(register)
+
+	if err != nil {
+		http.Error(w, "Ocurrio un error al intentar insertar el registro"+err.Error(), 400)
+	}
+
+	if !status {
+		http.Error(w, "No se logro  insertar el registro", 400)
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
